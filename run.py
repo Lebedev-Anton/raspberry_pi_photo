f5a4a1b204abf7f062729720c414bc8680ab8851
Антон Лебедев <antonlebedev561@gmail.com>
2022-03-24 22:29:45 +0300

    upd

--- run.py
+++ run.py
@@ -1,1 +1,36 @@
-from gphoto2_runner import main
+# test-server.py
+import socket
+import sys
+
+from gphoto2_runner.socket_communicator.CommandParser import command_parser
+import json
+
+# создаемTCP/IP сокет
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Привязываем сокет к порту
+server_address = ('localhost', 10001)
+sock.bind(server_address)
+
+# Слушаем входящие подключения
+sock.listen(1)
+
+while True:
+    # ждем соединения
+    connection, client_address = sock.accept()
+    try:
+        # Принимаем данные порциями и ретранслируем их
+        while True:
+            data = connection.recv(4096)
+            if data:
+                json_string = data.decode('utf-8')
+                json_data = json.loads(json_string)
+                answer = command_parser(json_data)
+                if answer == 'stop_server':
+                    sys.exit()
+                connection.sendall(answer)
+            else:
+                break
+    finally:
+        # Очищаем соединение
+        connection.close()
